fa15a7b77ee3d71d555754724a530348ab80a01a
severi12345 <severi.hirvonen@edu.riveria.fi>
2025-01-14 11:35:07 +0200

    kartta fixattu

--- src/Kartta.js
+++ src/Kartta.js
@@ -1,21 +1,26 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "index.css";
+import "leaflet/dist/leaflet.css";
 
 const Kartta = () => {
-  const position = [60.1699, 24.9384]; // Esimerkkinä Helsingin koordinaatit
+  // Joensuun keskustan koordinaatit
+  const joensuuPosition = [62.601, 29.763]; // Joensuun sijainti
+  const kylpylaPosition = [62.6122, 29.7601]; // Esimerkki: kylpylän sijainti Joensuussa
 
   return (
     <div style={{ height: "100vh", width: "100%" }}>
       <h2>Kartta</h2>
-      <MapContainer center={position} zoom={13} style={{ height: "90%", width: "100%" }}>
+      <MapContainer center={joensuuPosition} zoom={13} style={{ height: "90%", width: "100%" }}>
+        {/* OpenStreetMapin taustakartta */}
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        <Marker position={position}>
+        
+        {/* Marker kylpylän kohdalla */}
+        <Marker position={kylpylaPosition}>
           <Popup>
-            Tämä on Helsinki.
+            Tämä on kylpylä Joensuussa!
           </Popup>
         </Marker>
       </MapContainer>
