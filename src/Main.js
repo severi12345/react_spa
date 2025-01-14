176a445a5843ffdc59bd14e0d93e54851e94a172
severi12345 <severi.hirvonen@edu.riveria.fi>
2025-01-14 10:30:25 +0200

    luokat muunnettu nuolifunktioiksi

--- src/Main.js
+++ src/Main.js
@@ -1,33 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
 import {
-  BrowserRouter, Routes, Route,
+  BrowserRouter,
+  Routes,
+  Route,
   NavLink,
 } from "react-router-dom";
 import Koti from "./Koti";
 import Tarina from "./Tarina";
 import Yhteys from "./Yhteys";
- 
-class Main extends Component {
-    render() {
-      return (
-        <BrowserRouter>
-          <div>
-            <h1>Perus Single Page Application (SPA)</h1>
-            <ul className="header">
-              <li><NavLink exact to="/">Koti</NavLink></li>
-              <li><NavLink to="/tarina">Tarinaa</NavLink></li>
-              <li><NavLink to="/yhteys">Yhteys</NavLink></li>
-            </ul>
-            <div className="content">
-                <Routes>
-                    <Route path="/" element={<Koti />}/>
-                    <Route path="/tarina" element={<Tarina />}/>
-                    <Route path="/yhteys" element={<Yhteys />}/>
-                </Routes>
-            </div>
-          </div>
-        </BrowserRouter>
-      );
-    }
-  }
+
+const Main = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <h1>Perus Single Page Application (SPA)</h1>
+        <ul className="header">
+          <li><NavLink exact to="/">Koti</NavLink></li>
+          <li><NavLink to="/tarina">Tarinaa</NavLink></li>
+          <li><NavLink to="/yhteys">Yhteys</NavLink></li>
+        </ul>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Koti />} />
+            <Route path="/tarina" element={<Tarina />} />
+            <Route path="/yhteys" element={<Yhteys />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
 export default Main;
